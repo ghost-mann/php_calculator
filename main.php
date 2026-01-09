2c6f9c44934c6721ad8b178a534b8e224e8f779b
ghost-mann <iamaob8@protonmail.com>
2026-01-09 09:28:18 +0300

    feat: added html boiler plate in main.php

--- main.php
+++ main.php
@@ -1,1 +1,15 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple Calculator</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+
+</body>
+</html>
