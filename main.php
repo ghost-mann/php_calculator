06e97eff28831aed8fe758df5fa844e3a34248c9
ghost-mann <iamaob8@protonmail.com>
2026-01-09 11:51:38 +0300

    feat: added php conditional statement in main.php

--- main.php
+++ main.php
@@ -1,9 +1,30 @@
 <?php
-$num1 = $_POST['first'];
-$num2 = $_POST['second'];
-$operation = $_POST['operation'];
 
+$total = '';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $num1 = $_POST['first'];
+    $num2 = $_POST['second'];
+    $operation = $_POST['operation'];
+
+    if ($operation == '+') {
+        $total = $num1 + $num2;
+    } elseif ($operation == '-') {
+        $total = $num1 - $num2;
+    } elseif ($operation == '*') {
+        $total = $num1 * $num2;
+    } elseif ($operation == '/') {
+        if ($num2 == 0) {
+            echo "Cannot divide by zero";
+        } else {
+            $total = $num1 / $num2;
+        }
+    } else {
+        $error = "Invalid operation";
+    }
+
+}
 
 ?>
 
@@ -17,18 +38,26 @@ $operation = $_POST['operation'];
 <body>
 <h1>SIMPLE CALCULATOR</h1>
 <div class="container">
-    <form class="form-group">
+    <form class="form-group" method="post">
         <div class="input-boxes">
             <label for="first">Enter the First Number: </label><input id="first" name="first" type="number" placeholder="1..2...3...4"><br>
             <label for="second">Enter the Second Number: </label><input id="second" name="second" type="number" placeholder="5..6..7..8"><br>
-            <label for="total">TOTAL</label><input id="total" name="total" type="number" placeholder="The Answer is...">
+            <label for="total">TOTAL</label><input id="total" name="total" type="number" placeholder="The Answer is..." value="<?php echo $total; ?>">
         </div>
         <div class="operations">
             <select name="operation" required>
-                <option value="+" <?php echo (isset($_POST['operation']) && $_POST['operation'] == '+') ? 'selected' : ''?>>+</option>
-                <option value="-" <?php echo (isset($_POST['operation']) && $_POST['operation'] == '-') ? 'selected' : ''?>>-</option>
-                <option value="*" <?php echo (isset($_POST['operation']) && $_POST['operation'] == '*') ? 'selected' : ''?>>*</option>
-                <option value="/" <?php echo (isset($_POST['operation']) && $_POST['operation'] == '/') ? 'selected' : ''?>>/</option>
+                <option value="+" <?php echo (isset($_POST['operation']) && $_POST['operation'] == '+') ? 'selected' : '' ?>>
+                    +
+                </option>
+                <option value="-" <?php echo (isset($_POST['operation']) && $_POST['operation'] == '-') ? 'selected' : '' ?>>
+                    -
+                </option>
+                <option value="*" <?php echo (isset($_POST['operation']) && $_POST['operation'] == '*') ? 'selected' : '' ?>>
+                    *
+                </option>
+                <option value="/" <?php echo (isset($_POST['operation']) && $_POST['operation'] == '/') ? 'selected' : '' ?>>
+                    /
+                </option>
             </select>
             <button class="button" type="submit" id="button">SUBMIT</button>
         </div>
