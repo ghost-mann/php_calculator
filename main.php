e3159a3692a24501fc460154eda6f2599818386a
ghost-mann <iamaob8@protonmail.com>
2026-01-09 09:51:52 +0300

    feat: added input boxes and labels in main.php, added vertical column styling for input boxes and labels in styles.css

--- main.php
+++ main.php
@@ -10,6 +10,18 @@
 </head>
 <body>
 <h1>SIMPLE CALCULATOR</h1>
+<div class="container">
+    <form class="form-group">
+        <div class="input-boxes">
+            <label>Enter the First Number : </label>
+            <input id="first" name="first" type="number" placeholder="1..2...3...4"><br>
+            <label>Enter the Second Number : </label>
+            <input id="second" name="second" type="number" placeholder="5..6..7..8"><br>
+            <label>TOTAL: </label>
+            <input id="total" name="total" type="number" placeholder="The Answer is...">
+        </div>
+    </form>
+</div>
 
 </body>
 </html>
